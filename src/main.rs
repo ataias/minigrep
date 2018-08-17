66374cfae3cbda2dbb3f24dccf649030d84349a8
Ataias Pereira Reis <ataiasreis@gmail.com>
2018-08-17 06:49:33 -0300

    Use Result to indicate parsing failure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::env;
 use std::fs;
-use std::io::prelude::*;
+// use std::io::prelude::*;
+use std::process;
 
 fn main() {
     // collect gets an iterator and turns it into a collection
     // it needs to be annotated
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -23,10 +27,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+             return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
