c60967c65a9360528892a5314d32dc9bacb8ffd7
Ataias Pereira Reis <ataiasreis@gmail.com>
2018-08-17 06:15:15 -0300

    Accept command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // collect gets an iterator and turns it into a collection
+    // it needs to be annotated
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    println!("{:?}", args);
 }
